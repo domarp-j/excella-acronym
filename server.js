2d6ca4ae2d296ea00cb66ba147d496260c0360af
Pramod Jacob <psjacob68@gmail.com>
2017-03-14 19:03:17 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-//
-// Call packages
-//
+// ====================
+// Modules
+// ====================
 var express = require('express');
 var bodyParser = require('body-parser');
 
-//
-// Configure app to use bodyParser()
-//
+// ====================
+// App Setup
+// ====================
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-//
-// Set port using
-//
 var port = process.env.PORT || 8080;
 
-//
-// Set routes for API
-//
 var router = express.Router();
 
+// ====================
+// API Routes
+// ====================
 router.get('/', function(req, res) {
   res.json({ message: 'hello world!' });
 });
 
-app.use('/', router);
+// ====================
+// Register Routes
+// ====================
+app.use('/api', router);
 
+// ====================
+// Run Server
+// ====================
 app.listen(port);
