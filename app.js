c3acc2e74fd22b2697b5024b6bb65b753a29c39f
Pramod Jacob <psjacob68@gmail.com>
2017-03-14 09:59:14 -0400

    testing dotenv-save with push

---
+++ app.js
@@ -0,0 +1,13 @@
+//
+// Configure dotenv-safe to handle ENV variables
+//
+require('dotenv-safe').load();
+
+console.log(process.env.TEST);
+
+// var RtmClient = require('@slack/client').RtmClient;
+// var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
+//
+// var bot_token = process.env.SLACK_BOT_TOKEN || '';
+//
+// var rtm = new RtmClient(bot_token);
