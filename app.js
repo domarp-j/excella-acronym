6df2c71027e954dc9441a805f91a97563c284798
Pramod Jacob <psjacob68@gmail.com>
2017-03-14 12:51:14 -0400

    slack bot responds to calls

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
 require('dotenv-safe').load();
 
 //
+// Require modules
+//
+var _ = require('lodash');
+
+//
 // Slack DK for Node.js setup
 //
 var RtmClient = require('@slack/client').RtmClient;
@@ -11,6 +16,7 @@ var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 var bot_token = process.env.BOT_TOKEN || '';
+var bot_user_id = process.env.BOT_USER_ID || '';
 
 var rtm = new RtmClient(bot_token);
 
@@ -20,5 +26,11 @@ var rtm = new RtmClient(bot_token);
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   text = message.text || '';
   words = text.split(' ');
+
+  bot_called = _.includes(words, '<@' + bot_user_id + '>');
+
+  if (bot_called) {
+    rtm.sendMessage("You rang?", message.channel);
+  }
 })
 rtm.start();
