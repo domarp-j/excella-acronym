eb83abf4a5118e5649363c24ae560196a6b0f0bb
Pramod Jacob <psjacob68@gmail.com>
2017-03-14 18:31:26 -0400

    moved bot code to bot folder

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
-// BOT USER ------------------------------------------------------------------
+//
+// Call packages
+//
+var express = require('express');
+var bodyParser = require('body-parser');
 
-// //
-// // Configure dotenv-safe to handle ENV variables
-// //
-// require('dotenv-safe').load();
 //
-// //
-// // Require modules
-// //
-// var _ = require('lodash');
+// Configure app to use bodyParser()
 //
-// //
-// // Slack DK for Node.js setup
-// //
-// var RtmClient = require('@slack/client').RtmClient;
-// var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
-// var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-//
-// var bot_token = process.env.BOT_TOKEN || '';
-// var bot_user_id = process.env.BOT_USER_ID || '';
-//
-// var rtm = new RtmClient(bot_token);
-//
-// //
-// // Register messages & do stuff
-// //
-// rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-//   text = message.text || '';
-//   words = text.split(' ');
-//
-//   bot_called = _.includes(words, '<@' + bot_user_id + '>');
-//
-//   if (bot_called) {
-//     rtm.sendMessage("You rang?", message.channel);
-//   }
-// })
-// rtm.start();
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json()):
 
-// SLASH COMMAND -------------------------------------------------------------
+//
+// Set port
+//
+var port = process.env.port || 8080;
