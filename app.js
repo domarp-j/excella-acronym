439b1fe800af810a361b80cf80e01eb80b7e7a23
Pramod Jacob <psjacob68@gmail.com>
2017-03-14 13:19:19 -0400

    storing away slack bot for now, in favor of slash command

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
-//
-// Configure dotenv-safe to handle ENV variables
-//
-require('dotenv-safe').load();
+// BOT USER ------------------------------------------------------------------
 
+// //
+// // Configure dotenv-safe to handle ENV variables
+// //
+// require('dotenv-safe').load();
 //
-// Require modules
+// //
+// // Require modules
+// //
+// var _ = require('lodash');
 //
-var _ = require('lodash');
-
+// //
+// // Slack DK for Node.js setup
+// //
+// var RtmClient = require('@slack/client').RtmClient;
+// var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
+// var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 //
-// Slack DK for Node.js setup
+// var bot_token = process.env.BOT_TOKEN || '';
+// var bot_user_id = process.env.BOT_USER_ID || '';
 //
-var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-
-var bot_token = process.env.BOT_TOKEN || '';
-var bot_user_id = process.env.BOT_USER_ID || '';
-
-var rtm = new RtmClient(bot_token);
-
+// var rtm = new RtmClient(bot_token);
 //
-// Register messages & do stuff
+// //
+// // Register messages & do stuff
+// //
+// rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+//   text = message.text || '';
+//   words = text.split(' ');
 //
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  text = message.text || '';
-  words = text.split(' ');
+//   bot_called = _.includes(words, '<@' + bot_user_id + '>');
+//
+//   if (bot_called) {
+//     rtm.sendMessage("You rang?", message.channel);
+//   }
+// })
+// rtm.start();
 
-  bot_called = _.includes(words, '<@' + bot_user_id + '>');
-
-  if (bot_called) {
-    rtm.sendMessage("You rang?", message.channel);
-  }
-})
-rtm.start();
+// SLASH COMMAND -------------------------------------------------------------
