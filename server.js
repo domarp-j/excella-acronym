083f8d810469b5e99f1f207f6655993b40213ed6
Pramod Jacob <psjacob68@gmail.com>
2017-03-14 18:37:30 -0400

    basic server.js setup & moved relevantcode to slack_* folders

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ var bodyParser = require('body-parser');
 //
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json()):
+app.use(bodyParser.json());
 
 //
-// Set port
+// Set port using
 //
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
+
+//
+// Set routes for API
+//
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({ message: 'hello world!' });
+});
+
+app.use('/', router);
+
+app.listen(port);
