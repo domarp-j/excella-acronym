ef0e59e53aee840f24a1f88be736d35dbd57ff68
Pramod Jacob <psjacob68@gmail.com>
2017-03-24 10:28:04 -0400

    modified test script in package.json

--- test/acronym.js
+++ test/acronym.js
@@ -13,7 +13,7 @@ let mongoose = require('mongoose');
 //
 // Models
 //
-let Acronym = require('../../app/models/acronym');
+let Acronym = require('../app/models/acronym');
 
 //
 // Server
@@ -37,3 +37,48 @@ chai.use(chaiHttp);
 // ====================
 // Tests
 // ====================
+
+//
+// Test Data
+//
+let testData = [
+  {
+    name: 'ATM',
+    meaning: 'At The Moment',
+    createdAt: Date.now(),
+    updatedAt: Date.now()
+  }, {
+    name: 'ATM',
+    meaning: 'Automated Transaction Machine',
+    createdAt: Date.now(),
+    updatedAt: Date.now()
+  }, {
+    name: 'IRL',
+    meaning: 'In Real Life',
+    createdAt: Date.now(),
+    updatedAt: Date.now()
+  }, {
+    name: 'LOL',
+    meaning: 'Laughing Out Loud',
+    createdAt: Date.now(),
+    updatedAt: Date.now()
+  }
+]
+
+//
+// Testing
+//
+describe('Acronyms', () => {
+  //
+  // Seed database with test data before all tests
+  //
+  beforeEach((done) => {
+    Acronym.remove({}, (err) => {
+      testData.forEach((acronym, index) => {
+        Acronym.collection.insert(acronym).then(() => {
+          if (index === data.length - 1) done();
+        });
+      })
+    });
+  });
+})
