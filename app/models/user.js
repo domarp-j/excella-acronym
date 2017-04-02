acd75068f470113dc8fc6220b3a46f4d3a41ce42
Pramod Jacob <psjacob68@gmail.com>
2017-04-01 21:32:31 -0400

    added user model

---
+++ app/models/user.js
@@ -0,0 +1,27 @@
+// ====================
+// Modules
+// ====================
+
+let mongoose = require('mongoose');
+
+// ====================
+// Schema
+// ====================
+
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true
+  }
+);
+
+// ====================
+// Export
+// ====================
+
+module.exports = mongoose.model('User', UserSchema);
