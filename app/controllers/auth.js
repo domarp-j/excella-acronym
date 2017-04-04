414a78f93bdf19def33f85038ec691d30a65d7b9
Pramod Jacob <psjacob68@gmail.com>
2017-04-04 15:15:02 -0400

    added barebones auth controller

---
+++ app/controllers/auth.js
@@ -0,0 +1,16 @@
+// ====================
+// Models
+// ====================
+
+let User = require('../models/user');
+
+// ====================
+// RESTful Methods
+// ====================
+
+//
+// POST Create
+//
+exports.create = (request, response) => {
+  
+};
