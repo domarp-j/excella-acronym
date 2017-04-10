4e8a5b292d4d029661c4eaa03f4655007e0d72e7
Pramod Jacob <psjacob68@gmail.com>
2017-04-10 13:54:15 -0400

    working on auth controller

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -21,9 +21,44 @@ exports.create = (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.send({
       success: false,
-      message: 'Authorization is not properly defined. Please ensure that "email" & "password" parameters are valid.',
+      message: 'Authentication is not properly defined. Please ensure that "email" & "password" parameters are valid.',
       email: req.body.email || 'undefined',
       password: req.body.password ? '(defined)' : 'undefined'
+    });
+  } else {
+    User.findOne({ email: req.body.email }, (err, user) => {
+      if (err) {
+        res.send({
+          success: false,
+          message: err.errmsg
+        });
+      } else if (!user) {
+        res.send({
+          success: false,
+          message: 'Authentication failed. User was not found.'
+        });
+      } else {
+        user.checkPassword(req.body.password, (err, isMatch) => {
+          if (err) {
+            res.send({
+              success: false,
+              message: err.errmsg
+            });
+          } else if (!isMatch) {
+            res.send({
+              success: false,
+              message: 'Authentication failed. Incorrect password.'
+            })
+          } else {
+            let token = jwt.sign(user, process.env.JWT_SECRET, { algorithm: 'HS256' });
+            res.send({
+              success: true,
+              email: user.email,
+              token: `JWT ${token}`
+            });
+          }
+        });
+      }
     });
   }
 };
