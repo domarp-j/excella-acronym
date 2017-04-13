5896d851f398ffc7dce37135e69d8a34e60325ac
Pramod Jacob <psjacob68@gmail.com>
2017-04-13 16:05:29 -0400

    added slack helper

---
+++ app/helpers/slack.js
@@ -0,0 +1,10 @@
+// ====================
+// Helpers
+// ====================
+
+//
+// Check if Slack token & team ID match parameters on file
+//
+exports.match = (token, teamId) => {
+  return token === process.env.SLACK_TOKEN && teamId === process.env.SLACK_TEAM_ID;
+};
---
+++ test/helpers/slack.js
@@ -0,0 +1,64 @@
+// ====================
+// Third-Party Modules
+// ====================
+
+let chai = require('chai');
+
+require('dotenv-safe').load();
+
+// ====================gi
+// Internal Modules
+// ====================
+
+//
+// Helper
+//
+let slackHelper = require('../../app/helpers/slack');
+
+// ====================
+// Setup
+// ====================
+
+//
+// Assertions
+//
+let should = chai.should();
+
+// ====================
+// Test Parameters
+// ====================
+
+let token = process.env.SLACK_TOKEN;
+let teamId = process.env.SLACK_TEAM_ID;
+
+// ====================
+// Testing
+// ====================
+
+describe('Slack Helpers', () => {
+  beforeEach(done => {
+    token = process.env.SLACK_TOKEN;
+    teamId = process.env.SLACK_TEAM_ID;
+    done();
+  });
+
+  describe('match', () => {
+    it('should return true if token & teamId match params on file', done => {
+      let result = slackHelper.match(token, teamId);
+      result.should.be.true;
+      done();
+    });
+
+    it('should return false if token does not match', done => {
+      let result = slackHelper.match(`${token}+invalid`, teamId);
+      result.should.be.false;
+      done();
+    });
+
+    it('should return false if teamId does not match', done => {
+      let result = slackHelper.match(token, `${teamId}+invalid`);
+      result.should.be.false;
+      done();
+    });
+  });
+});
